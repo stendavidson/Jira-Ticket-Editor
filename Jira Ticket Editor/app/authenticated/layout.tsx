524a0e1506b8ec778ee6a075e55df12b81fcc6d7
stendavidson <95745725+stendavidson@users.noreply.github.com>
2025-05-18 18:38:46 +1000

    Add ticket context

--- Jira Ticket Editor/app/authenticated/layout.tsx
+++ Jira Ticket Editor/app/authenticated/layout.tsx
@@ -1,15 +1,22 @@
 'use client'
 
+// Import Styles
+import style from "./layout.module.scss";
+
 // Internal Imports
 import Navbar from "../../components/Navbar/Navbar"
+import { TicketProvider } from "@/contexts/TicketContext";
+import Ticket from "@/components/Ticket/Ticket";
 
 export default function AuthenticatedLayout({ children }: { children: React.ReactNode}){
   
   return (
-    <>
+    <TicketProvider>
       <Navbar/>
-      {children}
-    </>
+      <div className={style.main}>
+        {children}
+      </div>
+      <Ticket/>
+    </TicketProvider>
   )
-
 }
