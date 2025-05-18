8be8a8d6092227cc9ae9b695d17ac93e6bdb20f1
stendavidson <95745725+stendavidson@users.noreply.github.com>
2025-05-18 19:20:06 +1000

    Project Ticket Board Component

---
+++ Jira Ticket Editor/components/ProjectBoard/ProjectBoard.tsx
@@ -0,0 +1,117 @@
+
+// Import styles
+import style from "./ProjectBoard.module.scss";
+
+// External Imports
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { ExtendedProjectInterface } from "../../interfaces/ExtendedProjectInterface";
+import request from "../../lib/nothrow_request";
+import IDListInterface from "@/interfaces/IDListInterface";
+import TicketTile from "../TicketTile/TicketTile";
+
+
+export default function ProjectBoard(){
+
+  const searchParams = useSearchParams();
+  const [projectData, setProjectData] = useState<ExtendedProjectInterface|null>(null);
+  const [ticketIDs, setTicketIDs] = useState<{id : string}[]>([]);
+  
+  useEffect(() => {
+
+    if(!searchParams.has("project-id")){
+      return;
+    }
+
+    async function fetchProjectData(){
+
+      let projectData: ExtendedProjectInterface | null = null;
+
+      // URL Params
+      const url: URL = new URL("/proxy", window.location.origin);
+      url.searchParams.append("pathname", `/project/${searchParams.get("project-id")}`);
+
+      // User request
+      const response = await request(
+        url.toString(),
+        {
+          method: "GET",
+        }
+      );
+
+      if(response?.status.toString().startsWith("2")){
+        projectData = await response?.json();
+      }
+      
+      setProjectData(projectData);
+    }
+
+    fetchProjectData();
+
+  }, [searchParams]);
+
+
+  useEffect(() => {
+
+    async function fetchTicketIDs(){
+
+      let IDList: IDListInterface | null = null;
+      let IDs: {id : string}[] = []
+
+      // URL Params
+      const url: URL = new URL("/proxy", window.location.origin);
+      url.searchParams.append("pathname", "/search/jql");
+
+      // Post Body
+      const postBody = {
+        "fields": ["id"],
+        "jql": `project=${projectData?.id} AND NOT (status = "Closed" or status = "Closed Lost" or status = "Closed won" or status = "Resolved" or status = "Done" or status = "DONE (IN PROD)" or status = "DONE (IN UAT)" or status = Completed or status = Canceled) order by id desc`
+      }
+
+      // User request
+      const response = await request(
+        url.toString(),
+        {
+          method: "POST",
+          body: JSON.stringify(postBody),
+        }
+      );
+
+      if(response?.status.toString().startsWith("2")){
+        IDList = await response?.json();
+        IDs = IDList?.issues ? IDList?.issues : [];
+      }
+      
+      setTicketIDs(IDs);
+    }
+
+    fetchTicketIDs();
+
+  }, [projectData])
+
+  return (
+    <div className={style.projectBoard}>
+      <div className={style.titleSection}>
+        <h1>{projectData?.name}</h1>
+      </div>
+      <div className={style.ticketSection}>
+        <button className={style.create} type="button">Create</button>
+        <div className={style.boardWrapper}>
+          <div className={style.ticketBoard}>
+            {ticketIDs.length === 0 ? 
+              (
+                <TicketTile/>
+              ) 
+            :
+              ticketIDs.map((value: {id: string}) => {
+                return (
+                  <TicketTile ticketID={value.id} key={value.id}/>
+                )
+              })
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
