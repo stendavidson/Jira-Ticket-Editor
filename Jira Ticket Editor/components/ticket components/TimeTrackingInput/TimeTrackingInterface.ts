d06278af8df7f2f671d57f1300ad7c37d7f29e07
stendavidson <95745725+stendavidson@users.noreply.github.com>
2025-07-14 00:24:02 +1000

    Removing redundant files

--- Jira Ticket Editor/components/ticket components/TimeTrackingInput/TimeTrackingInterface.ts
+++
@@ -1,9 +0,0 @@
-
-export interface TimeTrackingInterface {
-  originalEstimate: string;
-  remainingEstimate: string;
-  timeSpent: string;
-  originalEstimateSeconds: number;
-  remainingEstimateSeconds: number;
-  timeSpentSeconds: number;
-}
