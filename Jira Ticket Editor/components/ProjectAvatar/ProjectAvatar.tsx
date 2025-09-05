5e161b5edf62b59524836e4127e0ec7552b0e723
stendavidson <95745725+stendavidson@users.noreply.github.com>
2025-09-05 14:48:57 +1000

    Updated and Refactored Project Avatar

--- Jira Ticket Editor/components/ProjectAvatar/ProjectAvatar.tsx
+++ Jira Ticket Editor/components/ProjectAvatar/ProjectAvatar.tsx
@@ -4,16 +4,12 @@
 import styles from "./ProjectAvatar.module.scss";
 
 
-/**
- * This component displays the project avatar
- */
-export default function UserAvatar({ avatarURL, displayName, className }: { avatarURL: string|undefined, displayName: string|undefined, className: string }) {
+export default function ProjectAvatar({ avatarURL, displayName, className }: { avatarURL: string | undefined, displayName: string | undefined, className: string }) {
 
   return (
     <>
       <div className={`${styles.avatarCircle} ${className}`}>
-          <img className={styles.avatarImg} src={avatarURL ? avatarURL: undefined} alt={displayName ? `The avatar for the project ${displayName}`: "Project avatar logo couldn't be loaded"}/> 
-          <p className={styles.avatarTooltip}>{displayName ? displayName: "Unknown"}</p>
+          <img className={styles.avatarImg} src={avatarURL ? avatarURL : "./../Transparent.png"} alt={displayName ? `The avatar for the project ${displayName}`: "Project avatar logo couldn't be loaded"}/> 
       </div>
     </>
   );
