f85dd6d93c609dccdc6093c745894a67ca35aa9b
stendavidson <95745725+stendavidson@users.noreply.github.com>
2025-09-05 14:48:34 +1000

    Updated & Refactored Navbar

--- Jira Ticket Editor/components/Navbar/Navbar.tsx
+++ Jira Ticket Editor/components/Navbar/Navbar.tsx
@@ -7,158 +7,131 @@ import styles from  "./Navbar.module.scss";
 import Link from "../Link/Link";
 import UserAvatar from "../UserAvatar/UserAvatar";
 import Settings from "../Settings/Settings";
-import request from "../../lib/nothrow_request";
-import AtlassianUser from "../../interfaces/AtlassianUserInterface";
+import request from "../../lib/NoExceptRequestLib";
+import UserInterface from "../../interfaces/UserInterface";
 
 // External Imports
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState, Suspense } from "react";
+import { UserContext } from "@/contexts/UserContext";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
-export default function Navbar(){
 
-  // Show Settings
+
+function Navbar(){
+
+  // Next.js hooks
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const currentPath = usePathname();
+
+  // State Values
   const [showSettings, setShowSettings] = useState<boolean>(false);
-
-  // Show Dropdown
   const [showDropdown, setshowDropdown] = useState<boolean>(false);
-  
-  // Account info
-  const [accountID, setAccountID] = useState<string | undefined>(undefined);
-
-  // Logged in status - higher level account
-  const [elevated, setElevated] = useState<boolean | null>(null); // changed to null
-
-  // Write access
-  const [writeAccess, setWriteAccess] = useState<boolean>(false);
-
-  // Selected tab
   const [selectedTab, setSelectedTab] = useState<number>(2);
 
-  useEffect(() => {
-    //////////////////////////////////////////////////////////////
-    ////////////////////// Fetch user data ///////////////////////
-    //////////////////////////////////////////////////////////////
-    const fetchUser = async () => {
-
-      let user: AtlassianUser | null = null;
-
-      // URL Params
-      const url: URL = new URL("/proxy-api", window.location.origin);
-      url.searchParams.append("pathname", "/myself");
-
-      // User request
-      const response = await request(
-        url.toString(),
-        {
-          method: "GET",
-        }
-      );
-
-      if(response?.status.toString().startsWith("2")){
-        user = await response?.json();
-      }
-
-      // Set component variables & trigger re-render
-      setAccountID(user?.accountId);
-    }
-
-    fetchUser();
-
-    //////////////////////////////////////////////////////////////
-    ////////////////////// Validate Higher Level Auth ////////////
-    //////////////////////////////////////////////////////////////
-    const fetchElevated = async () => {
-
-      // URL Params
-      const url: URL = new URL("/internal/check-elevated", window.location.origin);
-
-      // User request
-      const response = await request(
-        url.toString(),
-        {
-          method: "GET",
-        }
-      );
-
-      // Set component variables & trigger re-render
-      setElevated(response?.status === 204);
-    }
-
-    fetchElevated();
+  // Context(s)
+  const userContext = useContext(UserContext);
 
 
-    //////////////////////////////////////////////////////////////
-    ////////////////////// Validate Write Access /////////////////
-    //////////////////////////////////////////////////////////////
-    const fetchWriteAccess = async () => {
-
-      // URL Params
-      const url: URL = new URL("/internal/check-write-access", window.location.origin);
-
-      // User request
-      const response = await request(
-        url.toString(),
-        {
-          method: "GET",
-        }
-      );
-
-      // Set component variables & trigger re-render
-      setWriteAccess(response?.status === 204);
-    }
-
-    fetchWriteAccess();
-    
-  }, [])
+  ////////////////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////// API Calls //////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////
 
 
-  useEffect(() => {
-
-    if (elevated !== null){
-      
-      const displaySettings = () => {
-        // URL
-        const url = new URL(window.location.href);
-
-        // Display Settings Window
-        if(url.searchParams.get("settings") === "true"){
-          setShowSettings(true);
-        }
-      }
-
-      displaySettings();
-    } 
-  }, [elevated]); // Run this effect when `elevated` changes
-
-
-  function clickSettings() {
-
-    // Update URL Params
-    const url = new URL(window.location.href);
-
-    if(showSettings){
-      url.searchParams.delete("settings");
-    }else{
-      url.searchParams.set("settings", "true");
-    }
-    
-    window.history.replaceState(null, '', url.toString());
-
-    setShowSettings(prev => !prev)
-  }
-
-  useEffect(() => {
+  /**
+   * This function retrieves & sets the current user's information
+   */
+  async function fetchUser(){
 
     // URL Params
-    const url = new URL(window.location.href);
+    const url: URL = new URL("/proxy-api", window.location.origin);
+    url.searchParams.append("pathname", "/myself");
+
+    // User request
+    const response = await request(
+      url.toString(),
+      {
+        method: "GET",
+      }
+    );
+
+    // Process response
+    let user: UserInterface | undefined = undefined;
+
+    if(response?.status.toString().startsWith("2")){
+      user = (await response?.json()) as UserInterface;
+    }
+
+    // Set component variables & trigger re-render
+    if(user && !userContext?.userData){
+      userContext?.setUserData(user);
+    }
+  }
+
+
+
+  ///////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////// Callbacks //////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////////////
+
+
+
+  /**
+   * This function is used to hide or display the settings component thereby setting
+   * or unsetting the relevant URL parameters.
+   */
+  function clickSettings() {
+
+    // Search parameters
+    const params = new URLSearchParams(searchParams.toString());
+
+    // Add or remove "settings=true" from query parameters
+    if (showSettings) {
+      params.delete("settings");
+    } else {
+      params.set("settings", "true");
+    }
+
+    // Shallow routing = update URL without page reload or getServerSideProps rerun
+    router.replace(`${currentPath}?${params.toString()}`);
+
+    setShowSettings((prev) => !prev);
+  }
+
+
+
+  ///////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////// Effects ///////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////////////
+
+
+
+  /**
+   * This effect performs initial state and data loading.
+   */
+  useEffect(() => {
+
+    // Search parameters
+    const params = new URLSearchParams(searchParams.toString());
     
     // Set the select tab from url params
-    if(url.pathname.startsWith("/authenticated/my-work")){
+    if(currentPath.startsWith("/authenticated/my-work")){
       setSelectedTab(1);
     }else{
       setSelectedTab(2);
     }
 
-  }, [])
+    // Display settings
+    if(params.has("settings", "true")){
+      setShowSettings(true);
+    }
+    
+    // Retrieve necessary data
+    fetchUser();
+    
+  }, []);
+
+
 
   return (
     <>
@@ -166,14 +139,23 @@ export default function Navbar(){
         <nav className={styles.myNav}>
           <Link className={`${styles.link} ${selectedTab === 1 ? styles.highlight : ""}`} href="/authenticated/my-work" onClick={() => {setSelectedTab(1)}}>My Work</Link>
           <Link className={`${styles.link} ${selectedTab === 2 ? styles.highlight : ""}`} href="/authenticated/projects" onClick={() => {setSelectedTab(2)}}>Projects</Link>
-          <UserAvatar accountID={accountID} display={() => {setshowDropdown(true)}} hide={() => {setshowDropdown(false)}} className={styles.avatar}/>
+          <UserAvatar className={styles.avatar} avatarURL={userContext?.userData?.avatarUrls?.["48x48"]} displayName={userContext?.userData?.displayName} display={() => {setshowDropdown(true)}} hide={() => {setshowDropdown(false)}}/>
           <div className={styles.dropDown} style={{display: showDropdown ? "flex": "none"}}>
             <div className={styles.dropButton} onClick={clickSettings}>Settings</div>
             <Link className={styles.dropButton} href="/internal/logout">Logout</Link>
           </div>
         </nav>
       </header>
-      <Settings onClick={clickSettings} showSettings={showSettings} elevated={elevated} setElevated={setElevated} writeAccess={writeAccess}/>
+      <Settings onClick={clickSettings} showSettings={showSettings}/>
     </>
+  );
+}
+
+
+export default function Header(){
+  return (
+    <Suspense fallback={<header className={styles.myHeader}><nav className={styles.myNav}></nav></header>}>
+      <Navbar/>
+    </Suspense>
   );
 }
